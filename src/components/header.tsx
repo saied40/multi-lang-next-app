fb04e73820d9455844fa37d52255c06983f5db12
saied40 <noah12.gm@gmail.com>
2025-06-23 07:28:09 +0300

    edit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,19 @@
 "use client";
+import { usePathname, useRouter } from "@/i18n/navigation";
 import { routing } from "@/i18n/routing";
 import { useParams } from "next/navigation";
 
 export default function Header() {
   const { lang } = useParams();
+  const pathname = usePathname();
+  const router = useRouter();
 
   if (!lang) return null;
 
   const handleLangChange = async (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { value } = e.target;
-    window.location.href.replace('/' + lang as string, '/' + value);
+    // window.location.href = `/${value}`;
+    router.replace(pathname, { locale: value });
   };
 
   return (
